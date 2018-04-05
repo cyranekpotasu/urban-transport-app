f16452ebd242d4ccd4e9ce54e3693100a7c3561f
Michał <michal@Macbook-Micha.local>
2018-04-05 17:00:03 +0200

    Add graph implementation

---
+++ lib/graph.h
@@ -0,0 +1,33 @@
+#include<iostream>
+#include <list>
+ 
+using namespace std;
+ 
+
+class Graph
+{
+    int V;    
+ 
+    list<int> *adj;   
+public:
+    Graph(int V);  
+ 
+    
+    void addEdge(int v, int w); 
+ 
+  
+    void BFS(int s);  
+};
+ 
+Graph::Graph(int V)
+{
+    this->V = V;
+    adj = new list<int>[V];
+}
+ 
+void Graph::addEdge(int v, int w)
+{
+    adj[v].push_back(w); 
+}
+ 
+void Graph::addVertex()

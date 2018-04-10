8b55b07004112b3e3ae135d8ebdc32f15ff0a1a8
cyran <cyran.norbert97@gmail.com>
2018-04-10 22:58:49 +0000

    Build and test graph from csv files

--- main.cpp
+++ main.cpp
@@ -1,17 +1,46 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include "lib/graph.h"
+
+void load_tram_line(Graph& graph, std::string csv_filename) {
+    std::ifstream csv_file(csv_filename);
+    std::string line;
+    getline(csv_file, line);
+    getline(csv_file, line);
+    std::istringstream record(line);
+    std::string name, distance, longitude, latitude;
+    getline(record, name, ',');
+    getline(record, distance, ',');
+    getline(record, longitude, ',');
+    record >> latitude;
+    graph.add_vertex(name);
+
+    while(getline(csv_file, line)) {
+        std::istringstream record(line);
+        std::string prev_name(name);
+        getline(record, name, ',');
+        graph.add_vertex(name);
+        graph.add_edge(prev_name, name, atoi(distance.c_str()));
+        getline(record, distance, ',');
+        getline(record, longitude, ',');
+        record >> latitude;
+    }
+
+}
 
 int main() {
     Graph g;
-    g.add_vertex("Bema");
-    g.add_vertex("Hala targowa");
-    g.add_vertex("Dubois");
-    g.add_vertex("Botaniczny");
-    g.add_vertex("Pomorska");
-    g.add_edge("Bema", "Botaniczny", 2);
-    g.add_edge("Bema", "Dubois", 2);
-    g.add_edge("Dubois", "Pomorska", 1);
-    g.add_edge("Hala targowa", "Bema", 3);
+    std::vector<std::string> csv_files{};
+    std::ifstream csv_stream("../csv_files.txt");
+    while(!csv_stream.eof()) {
+        std::string file;
+        csv_stream >> file;
+        csv_files.push_back(file);
+    }
+    for(auto&& file: csv_files) {
+        load_tram_line(g, "../data/csv/" + file);
+    }
     g.print_graph();
 }
 

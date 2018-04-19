8ceb2eaca98fdacf8f006026f482270c3fdc15e2
cyran <cyran.norbert97@gmail.com>
2018-04-19 00:49:20 +0000

    Update plotting script

--- graph_draw.py
+++ graph_draw.py
@@ -42,9 +42,13 @@ if __name__ == '__main__':
     parser.add_argument('file', help='File with data to plot')
     args = parser.parse_args()
 
+    fig = plt.figure(frameon=False)
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
     draw_city_graph()
 
     longitudes, latitudes = get_path(args.file)
     plt.plot(longitudes, latitudes, marker='o', color='#d62728',
              linestyle='-')
-    plt.show()
+    plt.savefig('path.png')
